027d747fe048744baa629d99c503ba102943dbbc
staidigital <sondre.stai@gmail.com>
2023-08-24 21:18:02 +0200

    temp front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../app.css";
+</script>
+
+<main>
+    <span class="text-3xl font-mono text-fuchsia-300">Under utvikling</span>
+</main>
+
+<style>
+    :global(body){
+        margin: 0;
+    }
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        background-color: rgb(31, 31, 31);
+        border: 0;
+    }
+</style>
