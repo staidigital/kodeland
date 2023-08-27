38c3d34749d8a969db823b6e22105e4ec2a8e220
staidigital <sondre.stai@gmail.com>
2023-08-27 13:23:49 +0200

    vercel analytics and +layout setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+    import "../app.css";
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+
+    inject({ mode: dev ? 'development' : 'production' });
+</script>
+<nav class="flex justify-start h-10 items-center bg-gray-950">
+    <a href="/">Hjem</a>
+    <a href="/webutvikling">Webutvikling</a>
+    <!-- <a href="/prototyping">Prototyping</a>-->
+</nav>
+<slot />
+
+<style>
+    nav a {
+        margin: 0 1rem;
+        text-decoration: none;
+        font-size: 1rem;
+        font-weight: 350;
+        color: #fff;
+    }
+    nav a:hover {
+        @apply text-fuchsia-300;
+    }
+</style>
