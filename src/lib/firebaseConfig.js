12b2fbca93dfb95de40466077fef815d3112a1ff
staidigital <sondre@stai.dev>
2025-05-21 13:53:09 +0200

    removed firebase

--- src/lib/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBLxhHwk2ragTgzWZHphT5nnWjTh-O3dNA",
-  authDomain: "kodeland-95801.firebaseapp.com",
-  projectId: "kodeland-95801",
-  storageBucket: "kodeland-95801.appspot.com",
-  messagingSenderId: "946395047153",
-  appId: "1:946395047153:web:3909008728d94c65313550"
-};
-
